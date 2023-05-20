df99c5f4d9826f58d6bc1677eb75386d5eac4e7b
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2023-05-20 15:29:56 +0000

    feat: implement masonry teaser

--- components/ModuleTeaser.vue
+++ components/ModuleTeaser.vue
@@ -1,8 +1,15 @@
 <template>
-    <div class="module-teaser">
+    <div class="module-teaser" :class="{'module-teaser--masonry': fields.masonryGrid}">
       <h3 class="module-teaser__headline" v-if="fields.headline">{{  fields.headline }}</h3>
-      <div class="module-teaser__items">
+      <div v-if="!fields.masonryGrid" class="module-teaser__items">
         <TeaserItem v-for="teaser in fields.teaser" :key="teaser.sys.id" class="module-teaser__teaser-item" :title="teaser.fields.headline" :text="teaser.fields.text" :link="teaser.fields.link" :pageLink="teaser.fields.pageLink" :linkText="teaser.fields.linkText" />
+      </div>
+      <div v-if="!!fields.masonryGrid" class="module-teaser__items">
+        <masonry-wall :items="fields.teaser" :column-width="300" :gap="16">
+          <template #default="{ item, index }">
+            <TeaserBorderItem :image="index === 0" class="module-teaser__teaser-border-item" :title="item.fields.headline" :text="item.fields.text" :link="item.fields.link" :pageLink="item.fields.pageLink" :linkText="item.fields.linkText" />
+          </template>
+        </masonry-wall>
       </div>
     </div>
   </template>
@@ -24,9 +31,15 @@
 
       &__items {
         @include mq($min-width: $desktop) {
-          display: grid;
-          grid-template-columns: repeat(4, 1fr);
-          gap: 20px;
+          
+        }
+      }
+
+      &__teaser-border-item {
+        margin-bottom: 16px;
+
+        &:last-child {
+          margin-bottom: 0;
         }
       }
 
@@ -40,6 +53,25 @@
         &:last-child {
           margin-bottom: 0;
         }
+      }
+
+      &--masonry {
+        
+        .module-teaser {
+          &__headline {
+            margin-bottom: 40px;
+          }  
+        }
+
+        @include mq($min-width: $desktop) {
+          display: grid;
+          grid-template-columns: 1fr 2fr;
+
+          &__items {
+            display: block;
+          }
+        }
+
       }
 
     }
